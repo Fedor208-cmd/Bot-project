1081ddd04ea4a3d13a9e2b89a2c3aa1b7ca4033e
Fedor208-cmd <fedorfalev@gmail.com>
2026-02-01 18:50:35 +0300

    Update and rename start_and_registration_commands.py to start_command.py

---
+++ start_command.py
@@ -0,0 +1,10 @@
+from check_if_not_registered_func import check_if_not_registered
+
+
+async def start(update, context):
+    if check_if_not_registered(update, context):
+        await update.message.reply_text(
+            "Привет! Для начала работы необходимо зарегестрироваться. Введи команду /help для списка команд."
+        )
+    else:
+        await update.message.reply_text("Привет! Введи команду /help для списка команд.")
