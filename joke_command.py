f52b0c8f8c78f1705ddc7230b9148ffeb058f73e
Fedor208-cmd <fedorfalev@gmail.com>
2026-02-01 18:47:45 +0300

    Update joke_command.py

--- joke_command.py
+++ joke_command.py
@@ -1,7 +1,7 @@
 from config import url
 import requests
 
-async def joke(update, context):
+async def jokes(update, context):
     response = requests.get(url)
     joke = response.json()
     await update.message.reply_text(f"{joke["setup"]}\n"
