4147399aca806a0d2903b1f344eefdf47694e3e6
Fedor208-cmd <fedorfalev@gmail.com>
2026-02-01 18:47:02 +0300

    Update check_command.py

--- check_command.py
+++ check_command.py
@@ -4,22 +4,24 @@ from check_if_not_registered_func import check_if_not_registered
 
 counter_yes = 0
 counter_questions = 1
+name = ""
 
 async def check(update, context):
-    global counter_questions
+    global counter_questions, name
     if check_if_not_registered(update, context):
         await update.message.reply_text("Вы не зарегистрированы!")
         return ConversationHandler.END
     file = open("database_users.json", "r", encoding="utf-8")
     data = json.load(file)
     file.close()
-    await update.message.reply_text(data[str(update.message.from_user.id)][str(counter_questions)])
+    name = data[str(update.message.from_user.id)]["name"]
+    await update.message.reply_text(f"{name}, {data[str(update.message.from_user.id)][str(counter_questions)]}")
     counter_questions += 1
     return 2
 
 
 async def check_continuation(update, context):
-    global counter_questions, counter_yes
+    global counter_questions, counter_yes, name
     file = open("database_users.json", "r", encoding="utf-8")
     data = json.load(file)
     file.close()
@@ -30,12 +32,12 @@ async def check_continuation(update, context):
         return 2
     if answer == "да":
         counter_yes += 1
-    if counter_questions > len(data[str(update.message.from_user.id)]):
+    if counter_questions > len(data[str(update.message.from_user.id)]) - 2:
         await update.message.reply_text(
-            f"{int((counter_yes / len(data[str(update.message.from_user.id)])) * 100)}% выполнено!")
+            f"{int((counter_yes / (len(data[str(update.message.from_user.id)]) - 2)) * 100)}% выполнено!")
         counter_yes = 0
         counter_questions = 1
         return ConversationHandler.END
-    await update.message.reply_text(data[str(update.message.from_user.id)][str(counter_questions)])
+    await update.message.reply_text(f"{name}, {data[str(update.message.from_user.id)][str(counter_questions)]}")
     counter_questions += 1
     return 2
