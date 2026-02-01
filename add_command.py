34b09e4cef2ffee0c0ee15d474dd6ea099ccf544
Fedor208-cmd <fedorfalev@gmail.com>
2026-02-01 18:45:44 +0300

    Update add_command.py

--- add_command.py
+++ add_command.py
@@ -7,7 +7,11 @@ async def add(update, context):
     if check_if_not_registered(update, context):
         await update.message.reply_text("Вы не зарегистрированы!")
         return ConversationHandler.END
-    await update.message.reply_text("Какой вопрос вы бы хотели добавить?")
+    file = open("database_users.json", "r", encoding="utf-8")
+    data = json.load(file)
+    file.close()
+    name = data[str(update.message.from_user.id)]["name"]
+    await update.message.reply_text(f"{name}, какой вопрос Вы бы хотели добавить?")
     return 2
 
 async def add_complete(update, context):
@@ -18,7 +22,7 @@ async def add_complete(update, context):
     file.close()
     file = open("database_users.json", "w", encoding="utf-8")
     if task not in ctrl[usr].values():
-        ctrl[usr][str(len(ctrl[usr]) + 1)] = task
+        ctrl[usr][str(len(ctrl[usr]) - 1)] = task
         json.dump(ctrl, file, indent=4, ensure_ascii=False)
         file.close()
         await update.message.reply_text("Вопрос добавлен.")
